ca826d94b0ab8b70fc9db365606fdd569e220293
Emiliax16 <efpg1608@uc.cl>
2024-04-24 22:56:30 -0400

    chor: configuraciones iniciales

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // tailwind funcionará en todas estas extensiones
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
