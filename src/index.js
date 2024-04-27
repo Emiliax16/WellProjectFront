e08daeb9eb1571d4791ee9a100540cb2b6d4d845
jpvergaralb <jpvergaralb@uc.cl>
2024-04-27 01:25:12 -0400

    feat: aplicar y servir contexto desde el root de la aplicacion

--- src/index.js
+++ src/index.js
@@ -3,23 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Test from './Test';
-import reportWebVitals from './reportWebVitals';
+import Admin from './views/Admin';
+import View from './views/View';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(  
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/*" element="404 Not Found" />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/admin" element={ <Admin />} />
+          <Route path="/view" element={<View />} />
+          <Route path="/*" element="404 Not Found" />
+        </Routes>
+      </Router>
+    </AuthProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
