a8abbdb85cc970c804cf6c481543dcd5c478575c
jpvergaralb <jpvergaralb@uc.cl>
2024-05-28 19:47:14 -0400

    feat: agregar estado de admin al context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,11 @@
 
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState, useEffect, useCallback } from 'react';
 import {getClientDetails} from '../services/clientServices';
 import { baseUrl, auth } from '../utils/routes.utils';
 import useLoading from '../hooks/useLoading';
+import useAdminStatus from '../hooks/useAdminStatus';
 
 const AuthContext = createContext(null);
 
@@ -13,28 +14,28 @@ export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, loadingIcon, setLoading] = useLoading();
-    // eslint-disable-next-line no-unused-vars
+    const [isAdmin, setAdminStatus] = useAdminStatus();
     const [cookies, setCookie, removeCookie] = useCookies(['token']);
     const baseURL = baseUrl;
-
-    useEffect(() => {
-        const fetchUserDetails = async () => {
-            if (cookies.token) {
-                try {
-                    setLoading(true);
-                    const response = await getClientDetails(cookies.token);
-                    setUser(response);
-                } catch (error) {
-                    console.error('Failed to fetch user details:', error);
-                }
-                finally {
-                    setLoading(false);
-                }
+    
+    const fetchUserDetails = useCallback(async () => {
+        if (cookies.token) {
+            try {
+                setLoading(true);
+                const response = await getClientDetails(cookies.token);
+                setUser(response);
+                setAdminStatus(response);
+            } catch (error) {
+                console.error('Failed to fetch user details:', error);
+            } finally {
+                setLoading(false);
             }
-        };
+        }
+    }, [cookies.token, setAdminStatus, setLoading]);
+    
+    useEffect(() => {
         fetchUserDetails();
-    }, [baseURL, cookies.token, setLoading]);
-
+    }, [fetchUserDetails]);
 
     const login = async (email, password) => {
         try {
@@ -44,18 +45,24 @@ export const AuthProvider = ({ children }) => {
                 password
             });
             const { token, user } = response.data;
+            if (cookies.token) {
+                removeCookie('token', { path: '/' });
+            }
+
             if (token) {
                 setCookie('token', token, {
                     path: '/',
-                    expires: new Date(Date.now() + 604800000), // 7 ddias
+                    expires: new Date(Date.now() + 3600000), 
                     secure: true, 
                     httpOnly: false,  // por ahora false, cuando estemos en https cambiar a true
                     sameSite: 'Strict'
                   });
                 setUser(user);
+                setAdminStatus(user);
             }
         } catch (error) {
             console.error('Login failed:', error.response ? error.response.data : error);
+            throw error;
         } finally {
             setLoading(false);
         }
@@ -64,9 +71,10 @@ export const AuthProvider = ({ children }) => {
     const logout = () => {
         removeCookie('token', { path: '/' });
         setUser(null);
+        setAdminStatus(null);
     };
 
-    const value = { user, login, logout, loading, loadingIcon };
+    const value = { user, login, logout, loading, loadingIcon, isAdmin };
 
     return (
         <AuthContext.Provider value={value}>
