858361d285bec6367b4ec2d91fbf9ff67aec7971
jpvergaralb <jpvergaralb@uc.cl>
2024-05-28 19:52:01 -0400

    refactor: actualizar componente de rutas privadas para la logica de admin

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,14 +2,14 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
 function PrivateRoute({ children, roles }) {
-  const { user, loading, loadingIcon } = useAuth();
+  const { user, loading, loadingIcon, isAdmin } = useAuth();
 
   if (loading) {
     return <div>{loadingIcon}</div>;
   }
 
 
-  const roleCheck = user && roles.includes(user.roleId === 1 ? 'admin' : 'regular');
+  const roleCheck = user && roles.includes('admin') && isAdmin;
 
   return roleCheck ? children : <Navigate to="/" replace />;
 }
